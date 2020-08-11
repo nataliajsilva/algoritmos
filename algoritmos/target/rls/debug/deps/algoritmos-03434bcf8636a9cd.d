4ee7976c7d481c2163afd9aeef0d235c0341fc2f
nataliajsilva <nataliajsilva2@outlook.com>
2020-08-11 12:51:31 -0300

    criando projeto rust

---
+++ algoritmos/target/rls/debug/deps/algoritmos-03434bcf8636a9cd.d
@@ -0,0 +1,5 @@
+/Users/natasilva/repositories/algoritmos/algoritmos/target/rls/debug/deps/algoritmos-03434bcf8636a9cd.rmeta: src/main.rs
+
+/Users/natasilva/repositories/algoritmos/algoritmos/target/rls/debug/deps/algoritmos-03434bcf8636a9cd.d: src/main.rs
+
+src/main.rs:
