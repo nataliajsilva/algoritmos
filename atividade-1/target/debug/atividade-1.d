7f296bb645c60557d8474ba14772f9b08e092d4c
nataliajsilva <nataliajsilva2@outlook.com>
2020-08-12 18:32:34 -0300

    tirando arquivos do rust

--- atividade-1/target/debug/atividade-1.d
+++ atividade-1/target/debug/atividade-1.d
@@ -1,1 +1,1 @@
-/Users/natasilva/repositories/algoritmos/mentoria-algoritmos/atividade-1/target/debug/atividade-1: /Users/natasilva/repositories/algoritmos/mentoria-algoritmos/atividade-1/src/main.rs
+/Users/natasilva/repositories/algoritmos/atividade-1/target/debug/atividade-1: /Users/natasilva/repositories/algoritmos/atividade-1/src/main.rs
--- atividade-1/target/rls/debug/deps/atividade_1-5171de8a1bde7b06.d
+++ atividade-1/target/rls/debug/deps/atividade_1-5171de8a1bde7b06.d
@@ -1,5 +1,5 @@
-/Users/natasilva/repositories/algoritmos/mentoria-algoritmos/atividade-1/target/rls/debug/deps/atividade_1-5171de8a1bde7b06.rmeta: src/main.rs
+/Users/natasilva/repositories/algoritmos/atividade-1/target/rls/debug/deps/atividade_1-5171de8a1bde7b06.rmeta: src/main.rs
 
-/Users/natasilva/repositories/algoritmos/mentoria-algoritmos/atividade-1/target/rls/debug/deps/atividade_1-5171de8a1bde7b06.d: src/main.rs
+/Users/natasilva/repositories/algoritmos/atividade-1/target/rls/debug/deps/atividade_1-5171de8a1bde7b06.d: src/main.rs
 
 src/main.rs:
