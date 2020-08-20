4415772160269db8e719f3d99314f924bab57408
nataliajsilva <nataliajsilva2@outlook.com>
2020-08-20 17:09:37 -0300

    desenvolvimento da atividade 8

--- atividade-8/src/main.rs
+++ atividade-8/src/main.rs
@@ -8,6 +8,28 @@ Sendo o primeiro número da série 0, o segundo 1, e os subsequentes a soma dos 
 //Exibir os 13 primeiros números da sequência
 
 fn main() {
+
+    let quantidade_numeros : i32 = 13;
+    let mut contador : i32 = 0;
+    let mut numero_anterior : i32 = -1;
+    let mut numero_atual : i32 = 1;
+    let mut proximo_numero : i32 ;
+
+
+    println!("Sequência de Fibonacci:");
+
+    while contador < quantidade_numeros{
+
+        proximo_numero = numero_anterior + numero_atual;
+        numero_anterior = numero_atual;
+        numero_atual = proximo_numero;
+
+        println!("{}", proximo_numero);
+        contador = contador + 1;
+
+    }
+
+    
     
 
 }
---
+++ atividade-8/target/debug/atividade-8.d
@@ -0,0 +1,1 @@
+/Users/natasilva/repositories/algoritmos/atividade-8/target/debug/atividade-8: /Users/natasilva/repositories/algoritmos/atividade-8/src/main.rs
