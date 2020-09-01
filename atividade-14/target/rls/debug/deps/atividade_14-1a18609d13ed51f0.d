8959f0473a908f5e7cc425a992c4cd2935eb3a04
nataliajsilva <nataliajsilva2@outlook.com>
2020-09-01 12:38:04 -0300

    desenvolvimento da atividade 16

---
+++ atividade-14/target/rls/debug/deps/atividade_14-1a18609d13ed51f0.d
@@ -0,0 +1,5 @@
+/Users/natasilva/repositories/algoritmos/atividade-14/target/rls/debug/deps/atividade_14-1a18609d13ed51f0.rmeta: src/main.rs
+
+/Users/natasilva/repositories/algoritmos/atividade-14/target/rls/debug/deps/atividade_14-1a18609d13ed51f0.d: src/main.rs
+
+src/main.rs:
---
+++ atividade-14/target/rls/debug/deps/atividade_14-41e2b61ff42dd7a6.d
@@ -0,0 +1,5 @@
+/Users/natasilva/repositories/algoritmos/atividade-14/target/rls/debug/deps/atividade_14-41e2b61ff42dd7a6.rmeta: src/main.rs
+
+/Users/natasilva/repositories/algoritmos/atividade-14/target/rls/debug/deps/atividade_14-41e2b61ff42dd7a6.d: src/main.rs
+
+src/main.rs:
