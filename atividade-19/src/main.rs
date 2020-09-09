d92036eee263f0544d09a74f137bdc58decda577
nataliajsilva <nataliajsilva2@outlook.com>
2020-09-09 18:14:40 -0300

    corrigindo formatação atividade 19

--- atividade-19/src/main.rs
+++ atividade-19/src/main.rs
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-fn main() {
-    println!("Hello, world!");
-}
-=======
+
+
 /*Elaborar um programa para calcular o máximo divisor comum de dois números inteiros positivos, MDC(x, y), usando o algoritmo de Euclides. Este algoritmo é baseado no fato de que se o resto da divisão de x por y representado por r, for igual a zero, y é o MDC. Se o resto r for diferente de zero, o MDC de x e y é igual ao MDC de y e r. O processo se repete até que o valor do resto da divisão seja zero.*/
 
 use std::io;
@@ -48,5 +45,3 @@ fn main() {
             .expect("Falha ao converter o valor")
     }
 }
-
->>>>>>> develop
